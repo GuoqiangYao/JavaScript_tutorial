c5b693f0e8ee261e42959c8a651f579626907edd
GuoqiangYao <guoqiang.yuo@ZD-Automotive.de>
2023-03-07 14:51:57 +0100

    Create ArrowFunc.js
    basic note for js arrow func

---
+++ ArrowFunc.js
@@ -0,0 +1,15 @@
+// this is a js file note for browsing js arrow function 
+
+// Regular fucn
+hello = function(){
+    console.log("hello here")
+}
+
+// hello()
+
+// Arrow func
+hello = () =>{
+    console.log("here is arrow func format")
+}
+
+// hello()
